cba194110a9c8a96bd3bea596d4886b91cbde10e
patricksCommits <vanv1995@vandals.uidaho>
2017-06-18 23:23:38 -0700

    Custom views complete

---
+++ PEEK/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PEEK
+//
+//  Created by Patrick VanVorce on 6/18/17.
+//  Copyright © 2017 Maholeycow_Development. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
